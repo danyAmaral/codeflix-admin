307448d55b8a2390868fe12d6e3b4c33d107e797
Danielle Amaral <danyamaral27@gmail.com>
2025-01-31 17:19:44 -0300

    add testes

--- Codeflix.Catalog/src/Codeflix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTest.cs
+++ Codeflix.Catalog/src/Codeflix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTest.cs
@@ -1,24 +1,38 @@
 ﻿using Codeflix.Catalog.Application.Interfaces;
 using Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
 using Codeflix.Catalog.Domain.Entity;
+using Codeflix.Catalog.Domain.Exeptions;
 using Codeflix.Catalog.Domain.Repository;
+using Codeflix.Catalog.UnitTests.Domain.Entity.Category;
 using FluentAssertions;
 using Moq;
+using System.Threading.Tasks;
 using UseCases = Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
 
 namespace Codeflix.Catalog.UnitTests.Application.CreateCategory
 {
+    [Collection(nameof(CreateCatororyTestFixture))]
     public class CreateCategoryTest
     {
+        private readonly CreateCatororyTestFixture _fixture;
+
+        public CreateCategoryTest(CreateCatororyTestFixture createCatororyTestFixture)
+        {
+            _fixture = createCatororyTestFixture;
+        }
+
+
         [Fact(DisplayName = nameof(CreateCategory))]
         [Trait("Application", "CreateCategory - use cases")]
         public async void CreateCategory()
         {
-            var repositoryMock = new Mock<ICategoryRepository>();
-            var unitOfWorkMock = new Mock<IUnitOfWork>();
+
+            var unitOfWorkMock = _fixture.GetIUnitOfWorkMock();
+            var repositoryMock = _fixture.GetCategoryRepositoryMock();
+
             var userCase = new UseCases.CreateCategory(unitOfWorkMock.Object, repositoryMock.Object);
 
-            var request = new CreateCategoryRequest("category name", "Caterory Description", true);
+            var request = _fixture.GetCreateCategoryRequest();
 
             var response = await userCase.Handle(request, CancellationToken.None);
 
@@ -29,6 +43,45 @@ namespace Codeflix.Catalog.UnitTests.Application.CreateCategory
             response.Name.Should().Be(request.Name);
             response.Id.Should().NotBeEmpty();
             response.CreatedAt.Should().NotBeSameDateAs(default);
+        }
+
+
+        [Theory(DisplayName = nameof(ThrowWhenCantInstantieteAggregateAsync))]
+        [Trait("Application", "CreateCategory - use cases")]
+        [MemberData(nameof(GetInvalidRequests))]
+        public async Task ThrowWhenCantInstantieteAggregateAsync(CreateCategoryRequest request, string exceptionMessage)
+        {
+            var unitOfWorkMock = _fixture.GetIUnitOfWorkMock();
+            var repositoryMock = _fixture.GetCategoryRepositoryMock();
+
+            var userCase = new UseCases.CreateCategory(unitOfWorkMock.Object, repositoryMock.Object);
+
+            Func<Task> task = async() =>  await userCase.Handle(request, CancellationToken.None);
+
+            await task.Should()
+                    .ThrowAsync<EntityValidationException>()
+                    .WithMessage(exceptionMessage);
+        }
+
+        public static IEnumerable<object[]> GetInvalidRequests()
+        {
+            var fixture = new CreateCatororyTestFixture();
+            var invalidRequestList = new List<object[]>();
+
+            var invalidRequestInputShortName = fixture.GetCreateCategoryRequest();
+            invalidRequestInputShortName.Name = invalidRequestInputShortName.Name.Substring(0, 2);
+
+            invalidRequestList.Add(new object[] {
+                fixture.GetInvalidRequestShortName(),
+                "Name should be at least 3 characters long" 
+            });
+
+            invalidRequestList.Add(new object[] {
+                fixture.GetInvalidRequestTolongName(),
+                "Name should be less or equal 255 characters long"
+            });
+
+            return invalidRequestList;
         }
     }
 }
---
+++ Codeflix.Catalog/src/Codeflix.Catalog.UnitTests/Application/CreateCategory/CreateCatororyTestFixture.cs
@@ -0,0 +1,76 @@
+﻿using Codeflix.Catalog.Application.Interfaces;
+using Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
+using Codeflix.Catalog.Domain.Repository;
+using Codeflix.Catalog.UnitTests.Common;
+using Moq;
+
+namespace Codeflix.Catalog.UnitTests.Application.CreateCategory
+{
+    public class CreateCatororyTestFixture: BaseFixture
+    {
+        public string GetValidCategoryName()
+        {
+            var categoryName = "";
+            while (categoryName.Length < 3)
+                categoryName = Faker.Commerce.Categories(1)[0];
+            if (categoryName.Length > 255)
+                categoryName = categoryName[..255];
+
+            return categoryName;
+        }
+
+        public string GetValidCategoryDescription()
+        {
+            var categoryDescription =
+                Faker.Commerce.ProductDescription();
+            if (categoryDescription.Length > 10_000)
+                categoryDescription =
+                    categoryDescription[..10_000];
+
+            return categoryDescription;
+        }
+
+        public CreateCategoryRequest GetInvalidRequestShortName()
+        {
+            var invalidInputShortName = GetCreateCategoryRequest();
+            invalidInputShortName.Name = invalidInputShortName.Name.Substring(0, 2);
+
+            return invalidInputShortName;
+        }
+        public CreateCategoryRequest GetInvalidRequestTolongName()
+        {
+            var invalidInputTooLongName = GetCreateCategoryRequest();
+            var tooLongNameForCategory = Faker.Commerce.ProductName();
+            while (tooLongNameForCategory.Length <= 255)
+                tooLongNameForCategory = $"{tooLongNameForCategory} {Faker.Commerce.ProductName()}";
+            invalidInputTooLongName.Name = tooLongNameForCategory;
+            return invalidInputTooLongName;
+        }
+
+        public bool GetRandomBoolean()
+        {
+            return (new Random()).NextDouble() < 0.5;
+        }
+
+        public CreateCategoryRequest GetCreateCategoryRequest()
+        {
+            return new CreateCategoryRequest(GetValidCategoryName(), GetValidCategoryDescription(), GetRandomBoolean());
+        }
+
+
+        public Mock<ICategoryRepository> GetCategoryRepositoryMock()
+        {
+            return new();
+        }
+
+        public Mock<IUnitOfWork> GetIUnitOfWorkMock()
+        {
+            return new();
+        }
+    }
+
+    [CollectionDefinition(nameof(CreateCatororyTestFixture))]
+    public class CreateCatororyTestFixtureCollection : ICollectionFixture<CreateCatororyTestFixture>
+    {
+    }
+}
